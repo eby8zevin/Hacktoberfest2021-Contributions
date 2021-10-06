1f28b9ddef9eaeb1c22cb27b45cac107fc9fe51c
Reginaldo Rubens da Silva <reginaldorubens@gmail.com>
2021-10-06 15:08:19 -0300

    Include missing 'static' in method declaration
    Include missing 'static' in method declaration

--- Java/Prime.java
+++ Java/Prime.java
@@ -15,7 +15,7 @@ public class Prime {
         }
     }
     
-    private String checkIsPrime(int num) {
+    private static String checkIsPrime(int num) {
         for (int i = 2; i < Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return " is not Prime.";
